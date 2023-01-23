b98d89e64aa379842162c1052f429f4dbc18a690
bomoniyi <bomoniyi@chicagobooth.edu>
2023-01-23 23:53:49 +0000

    Installed Devise

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.3'
 
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use postgresql as the database for Active Record
@@ -56,14 +57,13 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # appdev Gems
-gem 'faker'
-gem 'devise'
 group :development do
   gem 'annotate'
   gem 'awesome_print'
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'bullet'
+  gem 'faker'
   gem 'htmlbeautifier'
   gem 'pry-rails'
   gem 'rack-canonical-host'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '6bcbebdb8111bcba88b8342afae0c47506a6d5146052a921b66e2414c87ede44ea9e806c16ffde4ca9c86aeaa04ab27eaaea22efc2245ea416985ee2782370e3'
+  # config.secret_key = '82432a946e3b66f362cfee70f40a26d3d858b06380bb984b082b42a717776e96e2d6fe3f1796fe8e01447092579412ad2417069bb78cc4673b45610f659dce9a'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '8db525c51180cfc2e6be2c863b8297cd6648ffffaf8b7f8229f69e724eff7040443da2c3212e5284e643dc673a4946ad49b824bb113fd225a6c4805333df9085'
+  # config.pepper = 'a4d624a131d094f2901ce678ffebb212596bfe670f1ed2a4b547718aff7fc390d702ed9f418948b5cc2d561ba0610868175bec3e75904668cd9ba586189ac767'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
