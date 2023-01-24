a2061451445b177583a5eaf6ca6fafb0e92f9aa9
bomoniyi <bomoniyi@chicagobooth.edu>
2023-01-24 05:30:57 +0000

    Fixed all models and migrates

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,15 @@ class Photo < ApplicationRecord
 
   has_many :comments
   has_many :likes
+  has_many :fans, through: :likes
+
+  validates :caption, presence: true
+  validates :image, presence: true
+
+  scope :by_likes, -> { order(likes_count: :desc) }
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+  def fan_list
+    @_fan_list ||= fans.pluck(:username).to_sentence
+  end
 end
